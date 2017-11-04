1f883e2eb14220a85f6aa5cf637da373cd036d80
Tom Marx <tom@Toms-iMac.local>
2017-11-04 21:17:55 +0100

    Init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function preload() {}
+
+function setup() {
+  createCanvas(600, 450);
+}
+
+function draw() {
+    background(150, 200, 150);
+
+    textAlign(CENTER);
+    text("OHGJ #132", width / 2, height / 2);
+}
